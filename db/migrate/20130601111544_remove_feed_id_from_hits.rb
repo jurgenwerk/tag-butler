8474ae36e4993b499a036e951f19d8ed35510d2e
matixmatix <matic@jurglic.si>
2013-06-20 22:57:17 +0200

    navbar responsive fix

--- db/migrate/20130601111544_remove_feed_id_from_hits.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFeedIdFromHits < ActiveRecord::Migration
-  def up
-    remove_column :hits, :feed_id
-  end
-
-  def down
-    add_column :hits, :feed_id, :integer
-  end
-end
--- db/migrate/20130601111929_remove_user_id_from_feeds.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromFeeds < ActiveRecord::Migration
-  def up
-    remove_column :feeds, :user_id
-  end
-
-  def down
-    add_column :feeds, :user_id, :integer
-  end
-end
